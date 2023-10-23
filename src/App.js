5cc689353456c49a115b47baebeb6e7974977fcd
hjk92go <113073075+hjk92go@users.noreply.github.com>
2023-10-23 16:58:59 +0900

    231023_ch03 sortComponents&listRender

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "hjk",
+    content: "hello",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "김현주",
+    content: "hi",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "현주김",
+    content: "hello world",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div>
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ src/DiaryList.jsx
@@ -0,0 +1,24 @@
+import DiaryItem from "./DiaryItem";
+
+const DiaryList = ({ diaryList }) => {
+  console.log(diaryList);
+  return (
+    <div className="DiaryList">
+      <h1>일기 리스트</h1>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => (
+          //하나의 객체에 포함된 모든 데이터를 스프레드 연산자를 통해서 다 전달한다.
+          <DiaryItem key={it.id} {...it} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+//undefined로 전달될것같은 props들을 기본적으로 설정할수 있는 기능
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+
+export default DiaryList;
