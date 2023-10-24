06fbfa421b58fab114c7f63e92971cf7451bdcb7
hjk92go <113073075+hjk92go@users.noreply.github.com>
2023-10-24 16:04:41 +0900

    231024_ch09 useMemo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-
-// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
@@ -22,7 +20,7 @@ function App() {
         author: it.email,
         content: it.body,
         emotion: Math.floor(Math.random() * 5) + 1,
-        created_date: new Date().getTime() + 1,
+        created_date: new Date().getTime(),
         id: dataId.current++,
       };
     });
@@ -32,7 +30,9 @@ function App() {
 
   //마운트 시점에 수행할 콜백함수에 getData라고 api를 호출하는 함수를 적용
   useEffect(() => {
-    getData();
+    setTimeout(() => {
+      getData();
+    }, 1500);
   }, []);
 
   // author, content, emotion을 Oncreate함수가 받아서 이데이터에 업데이트 시키려고함
@@ -44,7 +44,7 @@ function App() {
       content,
       emotion,
       created_date,
-      id: dataId.current++,
+      id: dataId.current,
     };
     //데이터아이디 +1되어야함
     dataId.current += 1;
@@ -53,8 +53,6 @@ function App() {
   };
 
   const onRemove = (targetId) => {
-    console.log(`${targetId}가 삭제되었습니다.`);
-
     //해당 id값을 제외한 나머지를 새로운 배열만듬
     const newDiaryList = data.filter((it) => it.id !== targetId);
     //만든 새로운 배열을 setData로 보내주면 삭제 완료
@@ -68,9 +66,43 @@ function App() {
     setData(data.map((it) => (it.id === targetId ? { ...it, content: newContent } : it)));
   };
 
+  //ch09_기분이 좋은 일기가 몇개인지 / 안좋은게 몇개인지 / 기분좋은 일기의 비율이 어떻게 되는지 / 이세가지 데이터를 구할예정
+
+  //리액트에서 이렇게 return을 가지고 있는 함수를 메모이제이션 해서 이런 연산을 최적화 하기위해 useMemo를 사용하게됨
+  //우리가 최적화 하고 싶은 메모이제이션함수를 감싸주면된다.
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("일 기 분 석 시 작");
+
+    //좋은 일기 => 데이터state에서 필터를 이용하여 감정점수가 3점인것을 추린다음 그것의 길이를 구하면 갯수가 나온다.
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+
+    //나쁜 일기 => 일기 전체갯수에서 좋은 일기 갯수를 빼면 나머지는 나쁜일기 갯수
+    const badCount = data.length - goodCount;
+
+    //좋은 일기의 비율 => 좋은일기갯수/전체일기갯수 *100
+    const goodRatio = (goodCount / data.length) * 100.0;
+
+    return { goodCount, badCount, goodRatio };
+    //데이터 랭쓰가 변화하지 않는이상 계산하지않고 반환함
+  }, [data.length]);
+
+  /**
+   * useMemo에서 자주하는 실수
+   * 위와 같이 useMemo를 사용하게 되면 더이상 이거는 함수가 아니게 된다.
+   * 왜냐하면 useMemo라는 기능은 어떤 함수를 전달을 받아서 무엇을 리턴하냐=>callback함수가 리턴하는 값을 그냥 리턴함
+   *
+   */
+
+  //getDiaryAnalysis함수를 지역함수로 만들었기때문에 다른 함수들(onEdit,onRemove,OnCreate...) 처럼 리턴전에 호출을 하도록 할 것
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div>
       <DiaryEditor onCreate={onCreate} />
+      <div>전 체 일 기 : {data.length}</div>
+      <div>기 분 좋 은 일 기 갯 수 : {goodCount}</div>
+      <div>기 분 나 쁜 일 기 갯 수 : {badCount}</div>
+      <div>기 분 좋 은 일 기 비 율 : {goodRatio}%</div>
       <DiaryList onEdit={onEdit} diaryList={data} onRemove={onRemove} />
     </div>
   );
