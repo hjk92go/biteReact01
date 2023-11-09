d832931cf39ee0220b49da08781b2d784d92e83f
hjk92go <113073075+hjk92go@users.noreply.github.com>
2023-11-09 16:43:27 +0900

    231109 ch10-3 areEqual 사용하기

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,22 +1,5 @@
 import React, { useEffect, useState } from "react";
-
-//버튼을 눌렸을때 A는 리렌더X, B는 O인 이유
-//obj.count에서 count는 1로 setCount하는 격 But 콜솔 출력됨 이유? prop인 obj가 객체이기 때문에
-// 객체를 비교할때 얕은 비교를 하기 때문에 문제 발생(값에 의한 비교X, 주소에 의한 비교를 하기때문(img01,02참고_객체들은 생성하자마자 메모리주소를 가짐 / 03처럼 선언된다면, 같은 메모리 값을 가지게됨))
-/**
-  
-  function MyComponent(props) { }
-  function areEqual(prevProps, nextProps) {
-
-  export default React.memo(MyComponent, areEqual);
-
-  areEqual라는 함수는 nextProps가 prevProps와 동일한 값을 가지면 true를 반환하고, 그렇지 않다면 false를 반환하게 안에서 코딩하고, 
-  결론적으로 비교하여 함수로서 사용 되기 때문에 기존에 얖은 비교하게 하지 않고, 여기서 깊은 비교를 구현하여 true를 반환하면 리렌더링을 안시킬수도 있고
-  false를 반환하면 렌더링을 시킬 수도 있다.
-
-
-
-  */
+//얕은 비교를 하지 않고 렌더링 최적화 하기
 
 const CounterA = React.memo(({ count }) => {
   useEffect(() => {
@@ -34,6 +17,19 @@ const CounterB = React.memo(({ obj }) => {
   return <div> {obj.count}</div>;
 });
 
+const areEqual = (prevProps, nextProps) => {
+  //return true; //이전 프랍이랑 현재 프랍이랑 일치 -> 리렌더링 X
+  //return false; //이전과 현재가 다르다 -> 리렌더링 O
+
+  if (prevProps.obj.count === nextProps.obj.count) {
+    return true;
+  }
+  return false;
+};
+
+//아래와 같이 사용
+const MemoizeCounterB = React.memo(CounterB, areEqual);
+
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
   const [obj, setObj] = useState({
@@ -50,7 +46,7 @@ const OptimizeTest = () => {
       </div>
       <div>
         <h2>Counter B</h2>
-        <CounterB obj={obj} />
+        <MemoizeCounterB obj={obj} />
         <button
           onClick={() =>
             setObj({
